98d5332e0b35b7fa1fdac0720eb1d25d5ad4563e
Белкина Кристина <70813325+BelRisa@users.noreply.github.com>
2021-05-21 03:11:57 +0300

    Create 4_4.cpp

---
+++ 4_4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<random>
+#include <ctime>
+
+
+void print(const std::vector<int>& v0){
+    for(auto i : v0)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::cout << "13. The sequence again: ";//13
+    replace_if(v3.begin(), v3.end(), [](int a){return (a < 0);}, 0);
+    print(v3);
+
+    std::cout << "14. The sequence without 0: ";//14 
+    v3.erase(remove(v3.begin(), v3.end(), 0), v3.end());
+    print(v3);
+
+    std::cout << "15. Reversed sequence 3: ";//15
+    reverse(v3.begin(), v3.end());
+    print(v3);
+
+    std::cout << "16. Top-3 max elements: ";//16 v3 
+    std::sort(v3.begin(), v3.end());
+    std::cout << *(v3.end() - 1) << " " << *(v3.end() - 2) << 
+        " " << *(v3.end() - 3) << std::endl;
+  
+  
+    return 0;
+}
